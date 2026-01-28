62eda917d5bf92143c28eba8981bd219028b3b17
dl-gzz <dlgzz@outlook.com>
2026-01-28 19:03:53 +0800

    fix: 恢复 tldraw CSS 导入方式，使用 globals.css 中的 @import

--- src/components/whiteboard/TldrawBoard.tsx
+++ src/components/whiteboard/TldrawBoard.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import { Tldraw } from 'tldraw';
-import 'tldraw/tldraw.css';
 import { customShapeUtils } from './shapes/registry';
 import BoardLogic from './BoardLogic';
 import Link from 'next/link';
